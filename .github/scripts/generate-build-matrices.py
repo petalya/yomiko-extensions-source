98e92e9ddd8c8f35d3ced3cf645ac33491ca8c4d
petalya <namedaymen@gmail.com>
2025-08-25 16:00:03 +0100

    Update script

--- .github/scripts/generate-build-matrices.py
+++ .github/scripts/generate-build-matrices.py
@@ -23,60 +23,7 @@ def run_command(command: str) -> str:
     return result.stdout.strip()
 
 def get_module_list(ref: str) -> tuple[list[str], list[str]]:
-    changed_files = run_command(f"git diff --name-only {ref}").splitlines()
-
-    modules = set()
-    libs = set()
-    deleted = set()
-    core_files_changed = False
-
-    for file in map(lambda x: Path(x).as_posix(), changed_files):
-        if CORE_FILES_REGEX.search(file):
-            core_files_changed = True
-        elif match := EXTENSION_REGEX.search(file):
-            lang = match.group("lang")
-            extension = match.group("extension")
-            if Path("src", lang, extension).is_dir():
-                modules.add(f':src:{lang}:{extension}')
-            deleted.add(f"{lang}.{extension}")
-        elif match := MULTISRC_LIB_REGEX.search(file):
-            multisrc = match.group("multisrc")
-            if Path("lib-multisrc", multisrc).is_dir():
-                libs.add(f":lib-multisrc:{multisrc}:printDependentExtensions")
-        elif match := LIB_REGEX.search(file):
-            lib = match.group("lib")
-            if Path("lib", lib).is_dir():
-                libs.add(f":lib:{lib}:printDependentExtensions")
-
-    def is_extension_module(module: str) -> bool:
-        if not (match := MODULE_REGEX.search(module)):
-            return False
-        lang = match.group("lang")
-        extension = match.group("extension")
-        deleted.add(f"{lang}.{extension}")
-        return True
-
-    if len(libs) != 0 and not core_files_changed:
-        modules.update([
-            module for module in
-            run_command("./gradlew -q " + " ".join(libs)).splitlines()
-            if is_extension_module(module)
-        ])
-
-    if os.getenv("IS_PR_CHECK") != "true" and not core_files_changed:
-        with Path(".github/always_build.json").open() as always_build_file:
-            always_build = json.load(always_build_file)
-        for extension in always_build:
-            modules.add(":src:" + extension.replace(".", ":"))
-            deleted.add(extension)
-
-    if core_files_changed:
-        (all_modules, all_deleted) = get_all_modules()
-
-        modules.update(all_modules)
-        deleted.update(all_deleted)
-
-    return list(modules), list(deleted)
+    return get_all_modules()
 
 def get_all_modules() -> tuple[list[str], list[str]]:
     modules = []
